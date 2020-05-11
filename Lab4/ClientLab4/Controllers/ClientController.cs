cf5e11ebac5c976fdafd1576ce587b78108581a0
Juzi <nikita29071999@gmail.com>
2020-05-11 18:52:18 +0500

    add project

---
+++ Lab4/ClientLab4/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceReference;
+
+namespace Client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        ServiceClient service = new ServiceClient();
+
+        [HttpGet]
+        public string Get()
+        {
+            var response = service.GetDataAsync().Result;
+            return JsonSerializer.Deserialize<Dictionary<string, string>>(response).First().Value.Replace(' ', '_');
+        }
+
+    }
+}
---
+++ Lab4/Service/App_Code/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+// ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Реструктуризация" можно использовать для одновременного изменения имени класса "Service" в коде, SVC-файле и файле конфигурации.
+public class Service : IService
+{
+    public async Task<string> GetDataAsync()
+    {
+        var link = "http://www.mocky.io/v2/5c7db5e13100005a00375fda";
+        HttpClient http = new HttpClient();
+        HttpResponseMessage response = await http.GetAsync(link);
+        return await response.Content.ReadAsStringAsync();
+    }
+}
